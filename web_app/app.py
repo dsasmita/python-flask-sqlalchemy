8832140c061923f5455895998997547851cef5d1
Dadang Sasmita <dangs.work@gmail.com>
2017-12-23 20:54:41 +0700

    Templating with Bootstrap and Jinja

--- web_app/app.py
+++ web_app/app.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("settings.py")
 
     @app.route('/')
     def index():
-        return render_template("index.html")
+        return render_template("index.html", TITLE = "Home page for flask")
 
 
     @app.route('/about')
     def about():
-        return 'About me'
+        return render_template("about.html", TITLE = "This is me")
 
     return app
