f8a0a1afe811ec87591b6592a70143721ab9f166
dsasmita <dangs.work@gmail.com>
2018-06-01 20:54:39 +0700

    Alembic

--- web_app/app.py
+++ web_app/app.py
@@ -1,34 +1,39 @@
 from flask import Flask, render_template
 import psycopg2
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("settings.py")
 
+    db = SQLAlchemy(app)
+
+    class Page(db.Model):
+        __tablename__ = 'page'
+        id = Column(Integer, primary_key=True)
+        title = Column(String)
+        content = Column(String)
+
+    db.create_all()
+
     @app.route('/')
     def index():
-        con = psycopg2.connect("dbname=pythonthusiast_flask user=devuser password=devpassword host=postgres")
-        cur = con.cursor()
+        page = Page.query.filter_by(id=1).first()
 
-        cur.execute("select * from page where id = 1;")
-        id, title = cur.fetchone()
-
-        con.close()
-
-        return render_template("index.html", TITLE = title)
-
+        return render_template("index.html", TITLE=page.title, CONTENT=page.content)
 
     @app.route('/about')
     def about():
         con = psycopg2.connect("dbname=pythonthusiast_flask user=devuser password=devpassword host=postgres")
         cur = con.cursor()
 
-        cur.execute("select * from page where id = 2;")
-        id, title = cur.fetchone()
+        cur.execute("select * from page;")
+        id, title, content = cur.fetchone()
 
         con.close()
 
-        return render_template("about.html", TITLE = title)
-
+        return render_template("about.html", TITLE=title)
 
     return app
--- web_app/settings.py
+++ web_app/settings.py
@@ -1,1 +1,3 @@
 DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://devuser:devpassword@postgres:5432/pythonthusiast_flask'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
